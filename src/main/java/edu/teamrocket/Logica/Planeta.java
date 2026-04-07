75e0c3d5626298b2c8e15496294317814ec2c3fa
joaq240606fer-collab <joaq240606fer@gmail.com>
2026-04-07 09:23:06 +0200

    feat(Planeta): :sparkles: Añadir enum planeta con datos reales
    Añadir enum planeta con datos reales

---
+++ src/main/java/edu/teamrocket/Logica/Planeta.java
@@ -0,0 +1,17 @@
+package edu.teamrocket.Logica;
+
+import java.util.EnumSet;
+
+public enum Planeta {
+    Mercurio(3.303e+23, 2.4397e6),
+    Venus(4.869e+24, 6.0518e6),
+    Tierra(5.976e+24, 6.37814e6),
+    Marte(6.421e+23, 3.3972e6),
+    Jupiter(1.9e+27, 7.1492e7),
+    Saturno(5.688e+26, 6.0268e7),
+    Urano(8.686e+25, 2.5559e7),
+    Neptuno(1.024e+26, 2.4746e7);
+
+public static final double G = 6.67300E-11;
+
+
